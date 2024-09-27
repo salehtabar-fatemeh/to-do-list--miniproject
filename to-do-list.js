1649f9db3e8323917b8f4c60e00fcb5e3a3beae0
fatemeh salehtabar <salehtabarf@gmail.com>
2024-09-27 12:18:05 +0330

    file js created and code script for to do list added

---
+++ to-do-list.js
@@ -0,0 +1,23 @@
+const taskInp = document.querySelector(".task-inp");
+        const addBtn = document.querySelector(".add-btn");
+        let list = document.querySelector(".list");
+        const tasks = ["Read js", "Do homework"];
+        function update(){
+            list.innerHTML = " ";
+            for(let i = 0; i<tasks.length; i++){
+                list.innerHTML+= `<li><input type="checkbox">${tasks[i]}
+                    <button onclick ="remove()">Delete task</button></li>`;
+            }
+        }
+        update();
+        function remove(indexOfItem){
+            tasks.splice(indexOfItem, 1);
+            update();
+        }
+        addBtn.addEventListener("click", ()=>{
+            const tasksTittle = taskInp.value;
+            tasks.push(tasksTittle);
+            update();
+            taskInp.value = " ";
+            taskInp.focus();
+        })
